c0d9d8dc3ab23500a20c9a9d696f639e51109a9f
David <david.krivoklatsky@student.ssnd.sk>
2025-11-03 16:05:21 +0100

    Update all packages to Next.js 15 and ESLint 9

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -1,11 +1,26 @@
-module.exports = {
-  extends: ['next', 'prettier'],
-  rules: {
-    '@next/next/no-html-link-for-pages': 'off',
-  },
-  parserOptions: {
-    babelOptions: {
-      presets: [require.resolve('next/babel')],
+import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+  ...compat.extends("next", "prettier"),
+  {
+    rules: {
+      '@next/next/no-html-link-for-pages': 'off',
     },
-  },
-};
+    parserOptions: {
+      babelOptions: {
+        presets: [require.resolve('next/babel')],
+      },
+    },
+  }
+];
+
+export default eslintConfig;
